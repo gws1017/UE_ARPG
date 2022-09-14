5e6558a23cc546ff5b24f1b8a1d2698c629a658e
gws1017 <gws1017@naver.com>
2022-09-14 18:56:26 +0900

    [UE5] Initial Commit

---
+++ Source/UE_ARPG/UE_ARPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_ARPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_ARPG, "UE_ARPG" );
